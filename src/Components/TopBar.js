8b0c8ed1114af1158cf77e6e8115baf2a265e5d7
Indra Pratama Putra <indra.symfonia@gmail.com>
2019-07-18 11:33:39 +0700

    update

---
+++ src/Components/TopBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+
+export default class TopBar extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar>test</AppBar>
+      </div>
+    );
+  }
+}
