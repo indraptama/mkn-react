6e63d89f240397e75e0074c7c7e449bf9f24584c
Indra Pratama Putra <indra.symfonia@gmail.com>
2019-07-18 21:00:32 +0700

    update

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,12 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-export default class TopBar extends Component {
-  render() {
-    return (
-      <div>
-        <AppBar>test</AppBar>
-      </div>
-    );
+// export default class TopBar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <AppBar>test</AppBar>
+//       </div>
+//     );
+//   }
+// }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
   }
-}
+}));
+
+const TopBar = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default TopBar;
